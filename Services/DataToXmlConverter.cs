bd52634de1cd1d9aff3c5e88a3ae088c01c376e2
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-05-15 16:57:00 +0400

    Сделал примерную реализацию интерфейса импорта и экспорта xml

---
+++ Services/DataToXmlConverter.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using System.Xml.Serialization;
+
+namespace WSSTest.Services;
+
+public class DataToXmlConverter : IDataToXmlConverter
+{
+	public string ExportXML(DbContext context)
+	{
+		//TODO:Привести к рабочему коду
+		var allData = context.ToList();
+		XmlSerializer serializer = new XmlSerializer(typeof(allData));
+		StringWriter stringWriter = new StringWriter();
+		
+		serializer.Serialize(stringWriter, allData);
+		return stringWriter.ToString();
+	}
+
+	public void ImportXML(string UserXml, DbContext context)
+	{
+		//TODO:Привести к рабочему коду
+		var allData = context.ToList();
+		using (Stream reader = new MemoryStream(Encoding.UTF8.GetBytes(UserXml)))
+		{
+			XmlSerializer serializer = new XmlSerializer(typeof(allData));
+			context.Entry(serializer.Deserialize(reader)).State = EntityState.Modified;
+		}
+		
+	}
+}
